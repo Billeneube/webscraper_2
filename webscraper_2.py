0ef9b64f17eac04c3e946879e7e73a524340b403
Kufooloo <robot2boy@icloud.com>
2020-02-06 13:23:35 -0800

    added a bit to webscraper

--- webscraper_2.py
+++ webscraper_2.py
@@ -1,2 +1,29 @@
 from bs4 import BeautifulSoup
-import requests
+import urllib.request
+f = open('htmlpage.txt', mode='w+')
+fp = urllib.request.urlopen("https://www.amazon.com/Best-Sellers/zgbs")
+mybytes = fp.read()
+i = False
+mystr = mybytes.decode("utf8")
+fp.close()
+html_doc=mystr
+soup = BeautifulSoup(html_doc, 'html.parser')
+x=f.read()
+if x == soup.prettify():
+  print("the same")
+else:
+  print("not the same")
+# while i == False:
+#   if x == soup.prettify():
+#     i =True
+#   else: 
+#     print("not the same")
+
+
+
+
+
+
+
+f.write(soup.prettify())
+f.close
