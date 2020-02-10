9a6df448520caed008e89c3070034808b4333b36
Jonah Wright <43395506+Kufooloo@users.noreply.github.com>
2020-02-10 13:35:50 -0800

    Update webscraper_2.py

--- webscraper_2.py
+++ webscraper_2.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
-<<<<<<< HEAD
 import urllib.request
+from htmlpage import url
+
 f = open('htmlpage.txt', mode='w+')
 fp = urllib.request.urlopen("https://www.amazon.com/Best-Sellers/zgbs")
 mybytes = fp.read()
@@ -10,21 +11,35 @@ fp.close()
 
 html_doc=mystr
 soup = BeautifulSoup(html_doc, 'html.parser')
-content=soup.prettify()
-f.write(content)
+# content=soup.prettify()
+# f.write(content)
+readLink = url
 
 
 
 
 
-score = soup.find('div',attrs={'class': 'zg_rankInfo'})
 link = soup.find('a', attrs={'class' : 'a-link-normal'})
 endlink = link['href']
 fulllink = 'amazon.com'+endlink
 print(link['href'])
 print(fulllink)
 
-f.write(soup.prettify())
+if fulllink != readLink:
+  print('they are not the same')
+else:
+  print("they are the same")
+
+f.write(fulllink)
 f.close
-=======
->>>>>>> 4a414c06a1e681a666f598510e2f0b14022b5bb1
+fp = urllib.request.urlopen('https://'+str(fulllink))
+mybytes = fp.read()
+i = False
+mystr = mybytes.decode("utf8")
+fp.close()
+
+html_doc=mystr
+soup2 = BeautifulSoup(html_doc, 'html.parser')
+title = soup2.find('span', attrs={'id' : 'productTitle'}).text
+title = str(title)
+print(title.lstrip().rstrip())
