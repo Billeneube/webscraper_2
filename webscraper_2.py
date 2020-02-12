5b3215bec7dd0a5e8c8f2730d35db3b44656e950
Jonah Wright <43395506+Kufooloo@users.noreply.github.com>
2020-02-12 12:57:38 -0800

    Update webscraper_2.py

--- webscraper_2.py
+++ webscraper_2.py
@@ -17,9 +17,9 @@ fp.close()
 
 html_doc=mystr
 soup = BeautifulSoup(html_doc, 'html.parser')
-<<<<<<< HEAD
+
 readLink = str(url)
-=======
+
 
 
 link = soup.find('a', attrs={'class' : 'a-link-normal'})
@@ -33,19 +33,11 @@ if fulllink != readLink.lstrip():
 else:
   print("they are the same")
 
-<<<<<<< HEAD
+
 f.write("url = '" + fulllink + "'")
 f.close
 
-=======
-if fulllink != readLink:
-  print('they are not the same')
-else:
-  print("they are the same")
 
-f.write(fulllink)
-f.close
->>>>>>> e3a7328c5a707ce2580eb8868537495753d35857
 fp = urllib.request.urlopen('https://'+str(fulllink))
 mybytes = fp.read()
 i = False
@@ -56,4 +48,3 @@ html_doc=mystr
 soup2 = BeautifulSoup(html_doc, 'html.parser')
 title = soup2.find('span', attrs={'id' : 'productTitle'}).text
 title = str(title)
-
